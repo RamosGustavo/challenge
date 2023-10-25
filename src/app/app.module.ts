6bc6f7d1c1f50639b1d7127d14f70d1d7f661dd8
RamosGustavo <gr4030077@gmail.com>
2023-10-25 15:28:41 -0300

    implement grid with INGNITE IU

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormularioComponent } from './pages/formulario/formulario.component';
 import { TablaComponent } from './pages/tabla/tabla.component';
 import { ModalComponent } from './pages/modal/modal.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
 import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
 import { HttpClientModule } from "@angular/common/http";
 import { 
+	IgxAvatarModule,
+	IgxBadgeModule,
+	IgxButtonModule,
 	IgxGridModule,
-	IgxInputGroupModule
+	IgxIconModule,
+	IgxInputGroupModule,
+	IgxProgressBarModule,
+	IgxRippleModule,
+	IgxSwitchModule
 } from "igniteui-angular";
+import { 
+	IgxSparklineCoreModule,
+	IgxSparklineModule
+} from "igniteui-angular-charts";
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
-
-
+//I keep the new line
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,10 +52,20 @@ import {
     NgxMaskPipe,
     IgxPreventDocumentScrollModule,
     HttpClientModule,
+    IgxAvatarModule,
+    IgxBadgeModule,
+    IgxButtonModule,
     IgxGridModule,
-    IgxInputGroupModule
+    IgxIconModule,
+    IgxInputGroupModule,
+    IgxProgressBarModule,
+    IgxRippleModule,
+    IgxSwitchModule,
+    IgxSparklineCoreModule,
+    IgxSparklineModule
   ],
   providers: [provideNgxMask(),],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
